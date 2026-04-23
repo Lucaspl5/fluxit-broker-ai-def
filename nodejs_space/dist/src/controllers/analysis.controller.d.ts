0a45b97dff0e4c27e6e2e203083b50fe66e469ce
Lucaspl5 <lucaspariente55@gmail.com>
2026-04-23 15:02:31 +0200

    Delete nodejs_space directory

--- nodejs_space/dist/src/controllers/analysis.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { SignalService } from '../services/signal.service';
-export declare class AnalysisController {
-    private signalService;
-    private readonly logger;
-    constructor(signalService: SignalService);
-    runAnalysis(body: {
-        api_key?: string;
-    }): Promise<any>;
-}
--- nodejs_space/dist/src/controllers/signals.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { SignalService } from '../services/signal.service';
-export declare class SignalsController {
-    private signalService;
-    private readonly logger;
-    constructor(signalService: SignalService);
-    getRecentSignals(limit?: string): Promise<any[]>;
-    getSignalsBySymbol(symbol: string, limit?: string): Promise<any[]>;
-}
